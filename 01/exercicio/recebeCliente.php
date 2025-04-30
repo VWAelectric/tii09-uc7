4a028c055ae15831c2f0f760b8dca4c32e03486d
Clodoaldo <cas_maquinas@hotmail.com>
2025-04-30 00:42:06 -0300

    Resolução do exercício proposto em sala formulário do cliente

--- 01/exercicio/recebeCliente.php
+++ 01/exercicio/recebeCliente.php
@@ -1,6 +1,11 @@
 <?php
-/*
-Nome
-Email
-Telefone
-*/
+
+$nome = $_POST ['nome'];
+$email = $_POST ['email'];
+$telefone = $_POST ['phone'];
+
+echo "Nome: " . $nome ."<br>";
+echo "E-mail: " . $email . "<br>";
+echo "Telefone: " . $telefone;
+
+
